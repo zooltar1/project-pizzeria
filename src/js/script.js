4670282dde02c3735f26132a3a456a57816b8e38
Rafał Otka <zooltar1@gmail.com>
2020-01-06 16:16:26 +0100

    add initAmountWidget - working but 543 line error

--- src/js/script.js
+++ src/js/script.js
@@ -539,8 +539,8 @@
     initAmountWidget(){
       const thisCartProduct = this;
 
-      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.wrapper.querySelector(select.cart.toggleTrigger));
-      thisCartProduct.AmountWidget.addEventListener('click', function(){
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidget); // ??
+      thisCartProduct.amountWidget.addEventListener('click', function(){
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
 
