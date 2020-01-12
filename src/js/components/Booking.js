4a341eb6fad79dc5970cfd8bfe60d90fe0430a35
Rafał Otka <zooltar1@gmail.com>
2020-01-12 21:53:05 +0100

    add Datepicker.js working version

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import {templates, select} from '../settings.js';
 import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
 
 class Booking{
   constructor(element){
@@ -28,6 +29,7 @@ class Booking{
 
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
 
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
 
   }
 
@@ -36,6 +38,7 @@ class Booking{
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
 
   }
 
---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,63 @@
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {select, settings} from '../settings.js';
+
+class DatePicker extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+
+    const thisWidget = this;
+
+    thisWidget.dom = {};
+
+    thisWidget.dom.wrapper = wrapper;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+
+    thisWidget.initPlugin();
+
+  }
+
+  initPlugin(){
+    const thisWidget = this;
+
+    thisWidget.minDate = new Date(thisWidget.value);
+
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+    // eslint-disable-next-line no-undef
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,
+      locale: {
+        firstDayOfWeek: 1 // start week on Monday
+      },
+      disable: [
+        function(date) {
+          return (date.getDay() === 1);
+        }
+      ],
+      onChange: function(selectedDates, dateStr) { // do poprawy
+        thisWidget.value = dateStr;
+      }
+    });
+
+  }
+
+  parseValue(value){
+    return value;
+
+  }
+
+  isValid(){
+    return true;
+  }
+
+  renderValue(){
+
+  }
+
+}
+
+export default DatePicker;
